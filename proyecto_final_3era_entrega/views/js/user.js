8c80147cfc0c4ab09fa7e81ef4d41e080691d8d9
MirkoPescio <mirkopes.4050@gmail.com>
2022-11-13 12:07:46 -0300

    3era-entrega-proyecto-final_commit-2

--- proyecto_final_3era_entrega/views/js/user.js
+++ proyecto_final_3era_entrega/views/js/user.js
@@ -5,6 +5,10 @@ const mongooseCache = require("mongoose-cache");
 const saltRounds = 10;
 
 const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    email: { type: String, required: true, unique: true },
+    phoneNumber: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
